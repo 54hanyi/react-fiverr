5ebd164e88bee07263313999b8ba82efedbbd0a0
54hanyi <tp6c04u4456@yahoo.com.tw>
2023-11-05 01:11:32 +0800

    Add base & html link

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-fiverr/',
   plugins: [react()],
   server: { // 將 port 指定為 3000
     port: 3000,
