24b73456fea8baf99c1cb09e01a7ef913e590442
54hanyi <tp6c04u4456@yahoo.com.tw>
2023-11-05 18:05:06 +0800

    Add comment of config

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
+  // mode - 開發模式。
+  // process.cwd() - 目前工作目錄的路徑。
+  // 環境文件的前綴，這裡為空字串表示載入所有環境變數。
+    return {
     base: env.VITE_BASE_URL || '/',
     plugins: [react()],
   };
 });
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   base: import.meta.env.VITE_BASE_URL,
-//   plugins: [react()],
-// });
